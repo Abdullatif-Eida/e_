4adc0af2975a55af1481fa6fe3382a414eb96b97
abdullatifEida <abdullatif@equivator.com>
2024-06-01 17:48:24 +0300

    init push

---
+++ android/app/src/main/kotlin/com/example/e_commerce/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.abc.project.e_commerce
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/models/sliders.dart
@@ -0,0 +1,22 @@
+class SliderModel {
+  String image;
+  bool? isPublished;
+
+  SliderModel({required this.image, this.isPublished});
+
+  // Factory constructor to create a SliderModel from JSON
+  factory SliderModel.fromJson(Map<String, dynamic> json) {
+    return SliderModel(
+      image: json['image'],
+      isPublished: json['is_published'] as bool?,
+    );
+  }
+
+  // Method to convert a SliderModel to JSON
+  Map<String, dynamic> toJson() {
+    return {
+      'image': image,
+      'is_published': isPublished,
+    };
+  }
+}
